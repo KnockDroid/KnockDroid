406d8c6bfba74c6a9c4cce424e09b7022ec19151
Danish Nayeem <idanish.org@gmail.com>
2021-12-04 11:35:47 +0300

    Routing added

--- Examples/Counter/home.js
+++ Examples/Counter/home.js
@@ -5,62 +5,17 @@ define( [], function(){
      */
     var Model = function(){
         let self = this;
-        self.age = ko.observable( 24 );
-        self.name = ko.observable( "Danish Nayeem" );
-        self.message = ko.pureComputed( function(){
-            return self.name() + " is " + self.age() + " years old."
-        } );
-        self.add = function(){
-            self.age( self.age()+1 );
-        }
     }
     /**
      * View Object
      */
     var View = {
         "Layout" : {
-            id : "root",
-            init : [ "Absolute", "FillXY,VCenter" ],
+            init : [ "Linear", "FillXY,Vertical" ],
             kids : {
-                "FAB" : {
-                    id : "counterFab",
-                    init : [ "add" ],
-                    bind : {
-                        event : {
-                            "SetOnTouch" : "add"
-                        }
-                    }
-                },
-                "Layout":{
-                    init : ["Linear", "FillXY,Vertical,HCenter"],
-                    kids : {
-                        "AppBar":{
-                            id : "appbar",
-                            init : ["APP NAME"],
-                        },
-                        "TextEditOutline" : {
-                            init : [0.95, "Left", "Type Here", true],
-                            methods : {
-                                "SetPadding" : [0.0, 0.02, 0.0, 0.02]
-                            },
-                            bind : {
-                                value : {
-                                    ev : "SetOnChange",
-                                    obs : "name"
-                                }
-                            }
-                        },
-                        "TextH5" : {
-                            id : "counterText",
-                            init : ["--", 0.95, -1, "Monospace,Multiline,Left" ],
-                            methods : {
-                                "SetPadding" : [0.02,0.02,0.02,0.02]
-                            },
-                            bind : {
-                                computed : "message"
-                            }
-                        }
-                    }
+                "RouterView" : {
+                    //RouterView is also a layout
+                    init : ["Linear", "FillXY,Vertical"]
                 }
             }
         }
@@ -68,9 +23,21 @@ define( [], function(){
     
     return {
         View : View,
-        /**
-         * Model should be an object and not a Class
-         */
-        Model : new Model()
+        /* Model should be an object and not a Class */
+        Model : new Model(),
+        /* Router property defines all the routes and its ViewModel file name */
+        Routes : [
+            {
+                default : true,
+                path : "about",
+                file : "about"
+            },
+            {
+                path : "contact"
+                /**
+                 * filename is assumed to be the same as path
+                 */
+            }
+        ]
     }
 } );
--- KnockDroid.js
+++ KnockDroid.js
@@ -1,14 +1,27 @@
 class KnockDroid{
-	constructor( host, home ){
+	constructor( home, host ){
 		let self = this;
         self.host = host ? host : "";
         self.home = home ? home : "home";
         self.counter = 0;
         self.root = {
+            vm : {},
             view : {},
             model : {},
             layout : {},
-            kids : {}
+            kids : {},
+            //Route properties
+            routes : {},
+            allRoutes : {},
+            defaultRoute : "",
+            currentRoute : {
+                path : "",
+                lay : false,
+                kids : {},
+                view : {},
+                model : {}
+            },
+            routerView : false
         }
         self.regex = {
             //obs : /<([^}]+)>/g,
@@ -19,8 +32,8 @@ class KnockDroid{
 
         //Load Home Module
 		require( [self.host + self.home + ".js"], vm=>{
-            self.root = vm;
-            self.renderRoot( self.root );
+            self.root.vm = vm;
+            self.renderRoot( self.root.vm );
         } );
 	}
 
@@ -32,6 +45,8 @@ class KnockDroid{
         self.root.kids = {}
         app.AddLayout( self.root.layout );
         self.renderKids( vm.View["Layout"].kids, vm.Model, self.root.layout, self.root.kids );
+        self.root.routes = self.root.vm.Routes ? self.root.vm.Routes : [];
+        self.routerInit();
     }
 
     renderKids( kids, model, parent, kidsContainer ){
@@ -39,13 +54,20 @@ class KnockDroid{
         let tmp;
         let id;
         for( let k in kids ){
-            tmp = kids[k];
-            self.counter = self.counter+1;
-            id = "kid_id_" + self.counter;
-            kidsContainer[id] = self.getUi( k, kids[k], model );
-            parent.AddChild( kidsContainer[id] );
-            if( tmp.kids )
-                self.renderKids( tmp.kids, model, kidsContainer[id], kidsContainer );
+            if( k=="RouterView" ){
+                if( !self.root.routerView ){
+                    self.root.routerView = self.getUi( "Layout", kids[k], {} );
+                    parent.AddChild( self.root.routerView );
+                }
+            }else{
+                tmp = kids[k];
+                self.counter = self.counter+1;
+                id = "kid_id_" + self.counter;
+                kidsContainer[id] = self.getUi( k, kids[k], model );
+                parent.AddChild( kidsContainer[id] );
+                if( tmp.kids )
+                    self.renderKids( tmp.kids, model, kidsContainer[id], kidsContainer );
+            }
         }
     }
     
@@ -82,14 +104,15 @@ class KnockDroid{
                     case "computed":
                         let computed = uiData.bind.computed;
                         model[computed].subscribe( function( newVal ){
-                            ui[textMethod.set]( model[computed]() );
+                            ui[textMethod.set]( newVal );
                         } );
                         ui[textMethod.set]( model[computed]() );
                     break;
                     case "value":
                         let val = uiData.bind.value;
                         model[val.obs].subscribe( newVal=>{
-                            ui[textMethod.set]( model[val.obs]() );
+                            if(ui[textMethod.get]()!=model[val.obs]())
+                                ui[textMethod.set]( model[val.obs]() );
                         } );
                         ui[textMethod.set]( model[val.obs]() );
                         //Two way
@@ -101,6 +124,13 @@ class KnockDroid{
                         for( let e in uiData.bind.event ){
                             ui[e]( model[uiData.bind.event[e]] );
                         }
+                    break;
+                    case "href":
+                        let ev = uiData.bind.href.ev;
+                        let path = uiData.bind.href.path;
+                        ui[ev]( function(){
+                            kd.navigate( path );
+                        } );
                     break;
                 }
             }
@@ -130,6 +160,59 @@ class KnockDroid{
                     get : "GetText"
                 }
             break;
+        }
+    }
+
+    routerInit(){
+        let self = this;
+        let routes = self.root.routes;
+        self.root.allRoutes = {};
+        routes.forEach( r=>{
+            self.root.allRoutes[r.path] = r;
+            if( r.default ){
+                self.root.defaultRoute = r.path;
+            }
+        } )
+        window.onhashchange = function(){
+            if( self.root.routerView )
+                self.root.routerView.Animate( "FadeOut" );
+            self.onNavigate();
+        }
+        if( self.root.defaultRoute )
+            self.navigate( self.root.defaultRoute );
+    }
+
+    navigate( path ){
+        path = path ? path : self.root.defaultRoute;
+        let self = this;
+        window.location = "#/" + path
+    }
+
+    onNavigate(){
+        let self = this;
+        let path = window.location.hash.split( "#/" ).join("");
+        if( self.root.currentRoute.path!=path ){
+            let file = self.root.allRoutes[path].file ? self.root.allRoutes[path].file : self.root.allRoutes[path].path;
+            if( file ){
+                //Load Home Module
+                require( [self.host + file + ".js"], vm=>{
+                    self.root.currentRoute.path = path;
+                    self.root.currentRoute.view = vm.View;
+                    self.root.currentRoute.model = vm.Model;
+                    if( self.root.currentRoute.lay && self.root.routerView )
+                        self.root.routerView.DestroyChild( self.root.currentRoute.lay );
+                    self.root.currentRoute.lay = self.getUi( "Layout", vm.View.Layout, vm.Model );
+                    self.root.currentRoute.kids = {}
+                    if( self.root.routerView )
+                        self.root.routerView.AddChild( self.root.currentRoute.lay );
+                    if( vm.View["Layout"].kids ){
+                        self.renderKids( vm.View["Layout"].kids, vm.Model, self.root.currentRoute.lay, self.root.currentRoute.kids );
+                    }
+                    self.root.routerView.Animate( "FadeIn" );
+                } );
+            }else{
+                app.ShowPopup( "Page not found" );
+            }
         }
     }
 }
